778df68d52b1f1b1e0f976caeeabaf53d5cae281
maniakalochka <prochorwolochoff@gmail.com>
2024-09-22 12:05:32 +0300

    Базовые файлы для FastAPI

--- db_config.py
+++ db_config.py
@@ -7,6 +7,9 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()  #  загружаем переменную окружения
 
+
+
+
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
@@ -16,6 +19,8 @@ DB_PORT = os.getenv("DB_PORT")
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
+
+
 
 # фабрика сессий
 async_session = sessionmaker(
@@ -32,3 +37,6 @@ async def get_db():
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+
+
